2be5ab95dfc97a08c4722b6a6488accf2bcf1bb5
GalicaYlber <galica.ylber@gmail.com>
2023-01-11 18:50:08 +0200

    updated signup.php

--- pages/signup.php
+++ pages/signup.php
@@ -1,8 +1,4 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
 <html>
 
 <head>
@@ -17,6 +13,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" href="../style.css">
+    <!-- <link rel="script" href="../index.js"> -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 
 <body>
@@ -30,7 +28,7 @@
                 <div class="nav-item"><a href="#">Animals</a></div>
                 <div class="nav-item"><a href="#">Tickets</a></div>
                 <div class="nav-item"><a href="#">About</a></div>
-                <div class="nav-item"><a href="#">Sign Up</a></div>
+                <div class="nav-item"><a href="signup.html">Sign Up</a></div>
             </div>
 
         </nav>
@@ -40,27 +38,44 @@
 
             </div>
             <div class="right">
-                <div class="form-overlay">
-                    <form action="" method="post">
+                <div class="form-overlay" id="signup-overlay">
+                    <form method="post">
                         <p>First Name</p>
-                        <input type="text" name="fname" id="fname-input">
+                        <input type="text" name="fname" id="fname-input" required>
 
                         <p>Last Name</p>
-                        <input type="text" name="lname" id="lname-input">
+                        <input type="text" name="lname" id="lname-input" required>
 
+                        <p>Email</p>
+                        <input type="email" name="reg-email" id="email-input" required>
+
+                        <p>Password</p>
+                        <input type="password" name="reg-pass" id="pass-input" required>
+
+                        <p>Confirm Password</p>
+                        <input type="password" name="reg-cpass" id="cpass-input" required>
+                        
+                        
+                        <div class="signup-submit">
+                            <button type="submit" class="signup-button" onclick="register_auth()">Sign up</button>
+                        </div>
+                    </form>
+
+                    <a onclick="goLogin()" id="login-link">Already a user? Log in instead</a>
+                </div>
+
+                <div class="form-overlay" id="login-overlay">
+                    <form action="" method="post">
                         <p>Email</p>
                         <input type="email" name="email" id="email-input">
 
                         <p>Password</p>
                         <input type="password" name="pass" id="pass-input">
-
-                        <p>Confirm Password</p>
-                        <input type="password" name="cpass" id="cpass-input">
                         
-                        <button type="submit" id="signup-button">Sign up</button>
-
+                        <div class="signup-submit">
+                            <button type="submit" class="signup-button">Log in</button>
+                        </div>
                     </form>
-
                 </div>
             </div>
         </div>
@@ -69,5 +84,22 @@
     </div>
 
 </body>
+
+<script>
+    $("signup-overlay").submit(function(e) {
+        e.preventDefault();
+        
+        $.ajax({
+            url: 'register-user.php',
+            type: 'POST',
+            data: $("signup-overlay").serialize(),
+            contentType: 'application/x-www-form-urlencoded',
+            success: function(response) {
+                // You can show the response from the server in the form of an alert
+                alert(response);
+            }
+        }); 
+    });
+</script>
 
 </html>
