c7aa54475467f0c585487ebfb623e9b2b1fe792c
Arita Ismajli <92359547+ismajliarita@users.noreply.github.com>
2023-01-11 19:21:53 +0200

    ye

---
+++ pages/habitatMap.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="panelHabitatMap">
+        <nav class="nav">
+
+            <img src="../media/logo.png" id="logo" />
+
+            <div class="nav-items">
+                <div class="nav-item"><a href="./pages/index.php">Home</a></div>
+                <div class="nav-item"><a href="#">Animals</a></div>
+                <div class="nav-item"><a href="#">Tickets</a></div>
+                <div class="nav-item"><a href="#">About</a></div>
+                <div class="nav-item"><a href="./pages/signup.html">Sign Up</a></div>
+            </div>
+
+        </nav>
+
+        <div class="habitatMap_innerPannel">
+            
+
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+</body>
+</html>
