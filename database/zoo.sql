78584ed41ff0615dcde14d61445c8dacfaf39514
RamadaniEdon <edramadani@york.citycollege.eu>
2023-01-30 22:29:53 +0200

    updated sql

--- database/zoo.sql
+++ database/zoo.sql
@@ -28,50 +28,64 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `animals` (
-  `id` int(20) NOT NULL,
-  `habitat` varchar(25) NOT NULL,
-  `name` varchar(25) NOT NULL,
-  `type` varchar(25) NOT NULL,
-  `age` int(4) NOT NULL,
-  `description` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `type` varchar(30) NOT NULL,
+  `age` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `habitat` varchar(20) NOT NULL,
+  `vet_help` int(11) NOT NULL,
+  `food_time` varchar(25) NOT NULL,
+  `water_time` varchar(25) NOT NULL,
+  `clean_time` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `animals`
 --
 
-INSERT INTO `animals` (`id`, `habitat`, `name`, `type`, `age`, `description`) VALUES
-(1, 'Forest', 'Grizzie', 'Grizzly Bear', 9, 'Strongest, most powerful animal since the extintion of dinosaurs. The sheer power of a female bear wildly overshadows the power of John Cena. The strength of a grizzly bear is incredibly powerful compared to humans. A grizzly can lift up to 1500 pounds and can reach speeds of up to 30 miles per hour. It also has powerful jaws and sharp claws that can crush bones and tear flesh. Its bite force is estimated to be around 1,200 pounds per square inch, making it one of the most powerful predators in the animal kingdom. Comparatively, the bite force of a human is only around 200 pounds per square inch, making it miniscule in comparison. Its muscular limbs ensure it can run up to 30 miles per hour, making it extremely fast and agile. The strength of a grizzly bear is no joke; it is a force to be reckoned with. Grizzly bear fortitudine est incredibilis. Eorum corpore robusto et dentibus acutis, sunt animalia formidolosa et terribilia. Adversarii eorum causam desperant, quia ursi habent robur incredibile et ad resistendum sunt paratissimi. In lucta, ursi sunt proficui et non cedunt facile. Eorum corpore robusto et dentibus acutis, sunt animalia formidolosa et terribilia. Adversarii eorum causam desperant, quia ursi habent robur incredibile et ad resistendum sunt paratissimi. In lucta, ursi sunt proficui et non cedunt facile. Eorum corpore robusto et dentibus acutis, sunt animalia formidolosa et terribilia. Adversarii eorum causam desperant, quia ursi habent robur incredibile et ad resistendum sunt paratissimi. In lucta, ursi sunt proficui et non cedunt facile. Eorum corpore robusto et dentibus acutis, sunt animalia formidolosa et terribilia. Adversarii eorum causam desperant, quia ursi habent robur incredibile et ad resistendum sunt paratissimi. In lucta, ursi sunt proficui et non cedunt facile.'),
-(2, 'Ocean', 'Nemo', 'Ocellaris Clownfish', 2, 'Nemo is placed in an aquarium in the office of dentist Philip Sherman in Sydney. He meets the \"Tank Gang\" led by Gill, a Moorish idol. The Tank Gang tell Nemo that he is to be given to Sherman\'s niece Darla, who killed her previous fish. Gill decides to help Nemo and devises an escape plan: Nemo can fit inside the aquarium\'s filter tube and must block it with a pebble, obliging Sherman to put the fish into plastic bags while he cleans the tank, and allow them to roll out the window and into the harbor. Nemo attempts to place the pebble, but fails and is almost killed. \r\n\r\nMarlin and Dory awaken, but the mask falls into a deep trench. Descending after the mask, they are soon pursued by an anglerfish. Dory memorizes the address on the goggles and they escape. The two disregard directions from a school of moonfish, taking what Marlin believes is a safer route. After being stung by a forest of jellyfish, they are knocked unconscious and awaken in the East Australian Current with a group of sea turtles, including Crush and his son, Squirt. The story of Marlin\'s quest is relayed across the ocean to Sydney, where a pelican named Nigel tells the Tank Gang. Nemo then succeeds in blocking the filter and soon the aquarium is covered in green algae.'),
-(3, 'Jungle', 'Mufasa', 'Lion', 56, 'Mufasa was born the first son of Ahadi and Uru, in Pride Rock, the eldest brother of Askari, and as the heir to the throne of Pride Rock, while his younger brother served as the leader of the Lion Guard. When the two were adolescents, Askari returned to Pride Rock after a solo patrol in the Outlands. He informed Mufasa of his victory over a rogue lion’s attempt to take over the Pride Lands. In response, Mufasa playfully nicknamed him \"Scar,\" after the facial wound he had received from the incident. However, Scar became increasingly jealous of his brother’s position, partially due to the venom from his wound, and tries to overthrow him by using his roar, but ends up losing it for doing such.'),
-(4, 'Arctic', 'Happy Feet', 'Penguin', 2, 'Penguins are highly social birds, living in large colonies of up to several thousand individuals. Most species of penguin live in the Southern Hemisphere and are native to countries such as New Zealand, Chile, Argentina, and Antarctica. Penguins are uniquely adapted to the cold temperatures of their habitats. They have thick coats of feathers and a layer of fat that helps to insulate their bodies. Penguins are incredibly efficient swimmers, able to reach speeds of up to 15 miles per hour! Penguins are also excellent divers, able to dive up to 200 meters deep in search of food. Penguins have a unique way of communicating with one another. They use a variety of vocalizations, body language, and postures to express themselves.'),
-(5, 'Desert', 'Aladin', 'Meerkat', 260, 'The meerkat or suricate is a small mongoose found in southern Africa. It is characterised by a broad head, large eyes, a pointed snout, long legs, a thin tapering tail, and a brindled coat pattern. The head-and-body length is around 24–35 cm, and the weight is typically between 0.62 and 0.97 kg.'),
-(7, 'Ocean', 'a', 'a', 2, 'a'),
-(8, 'Jungle', 'assd', 'asss', 2, '12ewdasd'),
-(9, 'Jungle', 'adasd', 'asdasd', 2, 'asdasd'),
-(10, 'Jungle', 'adasd', 'asdasd', 2, 'asdasd'),
-(11, 'Jungle', 'adasd', 'asdasd', 2, 'asdasd'),
-(12, 'Jungle', 'adasd', 'asdasd', 2, 'asdasd'),
-(13, 'Jungle', 'adasd', 'asdasd', 2, 'asdasd'),
-(14, 'Jungle', 'adasd', 'asdasd', 2, 'asdasd'),
-(15, 'Jungle', 'adasd', 'asdasd', 2, 'asdasd'),
-(16, 'Jungle', 'adasd', 'asdasd', 2, 'asdasd'),
-(17, 'Jungle', 'adasd', 'asdasd', 2, 'asdasd'),
-(18, 'Jungle', 'adasd', 'asdasd', 2, 'asdasd'),
-(19, 'Jungle', 'adasd', 'asdasd', 2, 'asdasd'),
-(20, 'Jungle', 'adasd', 'asdasd', 2, 'asdasd'),
-(21, 'Jungle', 'adasd', 'asdasd', 2, 'asdasd'),
-(22, 'Jungle', 'adasd', 'asdasd', 2, 'asdasd'),
-(23, 'Jungle', 'adasd', 'asdasd', 2, 'asdasd'),
-(24, 'Jungle', 'adasd', 'asdasd', 2, 'asdasd'),
-(25, 'Jungle', 'adasd', 'asdasd', 2, 'asdasd'),
-(26, 'Jungle', 'adasd', 'asdasd', 2, 'asdasd'),
-(27, 'Jungle', 'adasd', 'asdasd', 2, 'asdasd'),
-(28, 'Jungle', 'adasd', 'asdasd', 2, 'asdasd'),
-(29, 'Jungle', 'Manny', 'Panda', 12, 'Panda anda manda benanda'),
-(30, 'Jungle', 'Manny', 'Panda', 12, 'Panda anda manda benanda'),
-(31, 'Forest', 'Jake', 'Grizzly Bear', 5555, 'YESYESYEYSEYSEYSEYSEYSEYSEYSEY'),
-(32, 'Jungle', 'gjingellbell', 'Meerkat', 6, 'vdsdhfg ds fsd ');
+INSERT INTO `animals` (`id`, `name`, `type`, `age`, `description`, `habitat`, `vet_help`, `food_time`, `water_time`, `clean_time`) VALUES
+(17, 'Shimpanze', 'tiger', 20, 'haha bro t shtina me kesh a hahah', 'Desert', 1, '2023-01-30 20:09:51', '2023-01-30 21:20:08', 0),
+(18, 'Leart', 'Tiger', 21, 'haha bro t shtina me kesh a ahaha haha hahah  haah', 'Arctic', 0, '2023-01-30 20:14:51', '', 1),
+(19, 'Leart', 'tiger', 20, 'haha bro t shtina me kesh a hahah', 'Arctic', 1, '2023-01-30 20:14:47', '', 1),
+(20, 'Leart', 'tiger', 20, 'haha bro t shtina me kesh a hahah', 'Arctic', 0, '2023-01-30 20:14:53', '', 0),
+(21, 'Leart', 'tiger', 20, 'haha bro t shtina me kesh a hahah', 'Jungle', 0, '', '', 0),
+(22, 'Leart', 'tiger', 20, 'haha bro t shtina me kesh a hahah', 'Ocean', 0, '2023-01-30 20:16:09', '2023-01-30 20:16:33', 0),
+(23, 'Leart', 'tiger', 20, 'haha bro t shtina me kesh a hahah', 'Desert', 0, '', '', 0),
+(24, 'Leart', 'tiger', 20, 'haha bro t shtina me kesh a hahah', 'Forest', 0, '', '', 0),
+(25, 'Leart', 'tiger', 20, 'haha bro t shtina me kesh a hahah', 'Arctic', 0, '', '', 0),
+(26, 'Leart', 'tiger', 20, 'haha bro t shtina me kesh a hahah', 'Arctic', 0, '', '', 0),
+(27, 'Leart', 'tiger', 20, 'haha bro t shtina me kesh a hahah', 'Jungle', 0, '', '', 0),
+(28, 'Leart', 'tiger', 20, 'haha bro t shtina me kesh a hahah', 'Ocean', 0, '2023-01-30 20:21:05', '2023-01-30 20:16:38', 0),
+(29, 'Leart', 'tiger', 20, 'haha bro t shtina me kesh a hahah', 'Desert', 0, '', '', 0),
+(30, 'Leart', 'tiger', 20, 'haha bro t shtina me kesh a hahah', 'Forest', 0, '', '', 0),
+(31, 'Leart', 'tiger', 20, 'haha bro t shtina me kesh a hahah', 'Arctic', 0, '', '', 0),
+(35, 'Edon', 'Njeri', 20, 'asdf sdfs dfsadf ', 'Arctic', 0, '', '', 0),
+(37, 'Tringa', 'Zebra', 19, 'Tringa flet andaj han mut', 'Jungle', 0, '', '', 0),
+(38, 'Edon', 'Njeri', 19, 'sdfp', 'Arctic', 0, '', '', 0),
+(39, 'Tringa', 'Zebra', 19, 'Tringa flet andaj han mut', 'Jungle', 0, '', '', 0),
+(43, 'sdfs', 'Lion', 34, 'sdfsdfa sdf sdf sd fsd fsf d', 'Jungle', 0, '', '', 0),
+(44, 'Edon', 'Lion', 19, 'sadfasdf', 'Jungle', 0, '', '', 0),
+(46, 'sdf', 'Lion', 9, 'sdfa sd fs', 'Arctic', 0, '', '', 0),
+(47, 'Edon', 'Kali', 19, 'sdfsdfsdaf sdf sdf s', 'Arctic', 0, '', '', 0),
+(48, 'Gent', 'Penguin', 19, 'Me fjale nuk munesh me pershkru dicka qe nuk pershkruhet', 'Arctic', 0, '', '', 0),
+(49, 'Lola', 'Dog', 2, 'Lola is aritas dog', 'Forest', 0, '', '', 0),
+(50, 'Lola', 'Dog', 2, 'Lola is aritas dog', 'Forest', 0, '', '', 0),
+(51, 'Lola', 'Dog', 2, 'Lola is aritas dog', 'Forest', 0, '', '', 0),
+(52, 'e', 'e', 4, 'fsdf', 'Forest', 0, '', '', 0),
+(53, 'ssd', 'dsf', 2, 'sdfsd', 'Ocean', 0, '', '', 0),
+(54, 'Edon', 'Rama', 7, 'Edon Ramadani', 'Ocean', 0, '', '', 0),
+(55, 'Edon', 'Rama', 4, 'ajdjsfsh', 'Ocean', 0, '', '', 0),
+(56, 'Edon', 'Lion', 9, 'sdjfskdjf dfshdk ', 'Forest', 0, '', '', 0),
+(57, 'Edon', 'Lion', 9, 'sdjfskdjf dfshdk ', 'Forest', 0, '', '', 0),
+(58, 'sd ', 'sdf ', 9, 'dsdd', 'Ocean', 1, '2023-01-30 20:16:46', '2023-01-30 20:16:47', 0),
+(60, 'Pluto', 'Dog', 9, 'Plutoooooooooo', 'Arctic', 0, '', '', 0),
+(61, 'Edon', 'Dog', 9, 'heu', 'Jungle', 0, '', '', 0),
+(62, 'Ylber', 'Njeri', 19, 'Cimeri Edonit', 'Ocean', 0, '', '', 0),
+(63, 'Arbnor', 'Njeri', 19, 'Nje cimer ', 'Arctic', 0, '', '', 0),
+(64, 'E', 'd', 3, 'dfsdfa', 'Ocean', 0, '', '', 0),
+(65, 'Edon', 'Njeri', 19, 'Edon Ramadani nga gjilani', 'Arctic', 0, '', '', 0),
+(66, 'Edon', 'rama', 6, 'sdf asflajsd lfkasdj lfas', 'Jungle', 0, '', '', 0);
 
 -- --------------------------------------------------------
 
